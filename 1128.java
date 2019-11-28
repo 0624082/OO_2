03df36c2907a25b3f833ad4b1cb811ae515638d5
0624082 <55526976+0624082@users.noreply.github.com>
2019-11-28 14:50:49 +0800

    Create 1128.java

---
+++ 1128.java
@@ -0,0 +1,28 @@
+interface Shape{
+  abstract void getArea();
+}
+
+
+class Rectangle implements Shape{
+  double length;
+  double width;
+  public Rectangle(double l, double w){
+     length = l;
+     width = w;
+  }
+  public void getArea(){
+    System.out.println("area="length*width);
+  }
+}
+
+class Triangle implements Shape{
+  double base;
+  double height;
+  public Rectangle(double b, double h){
+     base = b;
+     height = h;
+  }
+  public void getArea(){
+    System.out.println("area="base*height);
+  }
+}
