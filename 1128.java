3efa9e3b6ac45f906e15bacbb5e64cb7750d7f16
0624082 <55526976+0624082@users.noreply.github.com>
2019-11-28 16:15:53 +0800

    Update 1128.java

--- 1128.java
+++ 1128.java
@@ -1,32 +1,72 @@
-interface Shape{
-  abstract void getArea();
+public class HelloWorld{
+
+     public static void main(String []args){
+        Rectangle rec = new Rectangle(5,10);
+        rec.getArea();
+        Triangle tri = new Triangle(5,10);
+        tri.getArea();
+     }
 }
+interface Shape{
+
+  abstract void getArea();
+
+}
+
+
+
 
 
 class Rectangle implements Shape{
+
   double length;
+
   double width;
+
   public Rectangle(double l, double w){
+
      length = l;
+
      width = w;
+
   }
+
   public String toString(){
-  return "area+="
+
+  return "Area = ";
+
   }
+
   
+
   public void getArea(){
-    System.out.println(length*width);
+
+    System.out.println("Rectangle ="length*width);
+
   }
+
 }
 
+
+
 class Triangle implements Shape{
+
   double base;
+
   double height;
-  public Rectangle(double b, double h){
+
+  public Triangle(double b, double h){
+
      base = b;
+
      height = h;
+
   }
+
   public void getArea(){
-    System.out.println(base*height);
+
+    System.out.println("Triangle ="base*height/2);
+
   }
+
 }
